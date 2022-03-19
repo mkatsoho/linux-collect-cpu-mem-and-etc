b0d0e48e79d77d3a13855f23c5b06d235f3b596f
陈永刚 <chenyg@yinhe.ht>
2022-03-19 17:32:57 +0800

    add new feature: collect per cpu usage

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # this
 
+## Prereq
+
+To collect per cpu usage, you should do like these for **only once**
+
+- 1. Start `top`, and press 1 to show each cpu usage, and then, 
+- 2. press `shift + w`, the current config is automatically saved to `$HOME/.toprc`, and then
+- 3. press `control + c` to exit
+
 
 ## How to run
 
@@ -12,7 +20,7 @@ bash ./collect.sh	# save in files /tmp/*.log
 ## TODOs
 
 - [x] collect overall cpu usage
-- [ ] collect individal cpu usage
-- [ ] collect memory useage
+- [x] collect individal cpu usage
+- [x] collect memory useage
 - [ ] collect process and subprocess numbers of my app
 
--- collect.sh
+++ collect.sh
@@ -2,12 +2,16 @@
 
 
 LogFile=${1:-/tmp/collect_cpu_usage.log}
+LogFile2=${1:-/tmp/collect_per_cpu_usage.log}
 
-echo "Save CPU usage to $LogFile"
+CpuNum=$(top -b -n1 -p 1 | grep '%Cpu' | wc -l)
+TailLineNum=$(expr $CpuNum + 2)
+
+
+echo "Save $CpuNum CPU(s) and Mem usage to $LogFile and $LogFile2"
 echo
 echo
 sleep 2
-
 
 
 
@@ -18,10 +22,12 @@ sleep 2
 while [ 2 -gt 1 ]
 do
 
-	CPU_USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1) 
-
+	CPU_USAGE=$(top -b -n2 -p 1 | grep "Cpu(s)\|Mem :\|Swap:" | tail -3)
 	DATE=$(date "+%Y-%m-%d %H:%M:%S")
 	CPU_USAGE="$DATE CPU: $CPU_USAGE"
-
 	echo $CPU_USAGE | tee -a $LogFile
+
+	DATE=$(date "+%Y-%m-%d %H:%M:%S")
+	# top -b -n2 -p 1 | grep "%Cpu\|Mem :\|Swap:" | tail -$TailLineNum | sed -e "s/^/$DATE /" 
+	top -b -n2 -p 1 | grep "%Cpu" | tail -$TailLineNum | sed -e "s/^/$DATE /" 
 done
