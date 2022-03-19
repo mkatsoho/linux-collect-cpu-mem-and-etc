9a119ac24b3ac56963cf64dec4247b5dffa8b8e3
陈永刚 <chenyg@yinhe.ht>
2022-03-19 17:39:37 +0800

    fix bug

--- collect.sh
+++ collect.sh
@@ -8,6 +8,10 @@ CpuNum=$(top -b -n1 -p 1 | grep '%Cpu' | wc -l)
 TailLineNum=$(expr $CpuNum + 2)
 
 
+echo "Must to setup Prereq, according README file"
+ls -l ~/.toprc
+
+
 echo "Save $CpuNum CPU(s) and Mem usage to $LogFile and $LogFile2"
 echo
 echo
@@ -15,19 +19,18 @@ sleep 2
 
 
 
-
-# CPU_USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
-
-
 while [ 2 -gt 1 ]
 do
 
+	# collect cpu, mem usage
 	CPU_USAGE=$(top -b -n2 -p 1 | grep "Cpu(s)\|Mem :\|Swap:" | tail -3)
 	DATE=$(date "+%Y-%m-%d %H:%M:%S")
 	CPU_USAGE="$DATE CPU: $CPU_USAGE"
 	echo $CPU_USAGE | tee -a $LogFile
 
+	# collect each cpu usage
 	DATE=$(date "+%Y-%m-%d %H:%M:%S")
 	# top -b -n2 -p 1 | grep "%Cpu\|Mem :\|Swap:" | tail -$TailLineNum | sed -e "s/^/$DATE /" 
-	top -b -n2 -p 1 | grep "%Cpu" | tail -$TailLineNum | sed -e "s/^/$DATE /" 
+	# top -b -n2 -p 1 | grep "%Cpu" | tail -$TailLineNum | sed -e "s/^/$DATE /" >> $LogFile2
+	top -b -n2 -p 1 | grep "%Cpu" |  sed -e "s/^/$DATE /" >> $LogFile2
 done
